b483d24d36da0d98861ae2d48ace9bba44db900e
VGibbsy <vignesh.s@aialz.com>
2020-08-01 10:51:06 +0530

    Largest of 3 numbers updated

--- Largest of 3 numbers.py
+++ Largest of 3 numbers.py
@@ -1,11 +1,15 @@
+# taking input from user
 num1=input('Enter the first number: ')
+# taking input from user
 num2=input('Enter the second number: ')
+# taking input from user
 num3=input('Enter the third number: ')
+# Comparing the given inputs
 if (num1 >= num2) and (num1 >= num3) :
+#storing the output in lnum
     lnum=num1
 elif (num2 >=num1) and (num2 >=  num3) :
     lnum=num2
 else:
     lnum=num3
 print('The largest of {},{},{} given numbers is {}'.format(num1,num2,num3,lnum))
-
