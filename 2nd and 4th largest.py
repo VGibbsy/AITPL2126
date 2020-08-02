8e3eb95566b66e5667d4ba03532d77bf67b4f4aa
VGibbsy <vignesh.s@aialz.com>
2020-08-02 23:42:41 +0530

    Assignment 1

--- 2nd and 4th largest.py
+++ 2nd and 4th largest.py
@@ -1,4 +1,4 @@
 x = [100,50,10,40,60,0,600,80]
 x.sort()
-print("second largest num is",x[-2])
-print("fourth largest num is",x[-4])
+print("second largest num is {}".format(x[-2]))
+print("fourth largest num is {}".format(x[-4]))
