577ebe4c5b1b3687e00c7520400fbd7a6e18a7a1
VGibbsy <vignesh.s@aialz.com>
2020-08-02 23:47:32 +0530

    Assignment 1

--- largest number in a list.py
+++ largest number in a list.py
@@ -1,3 +1,3 @@
-l1 = [1000, 60, 500, 50, 888]
-l1.sort()
-print("the largest no in list is {}".format(l1[-1]))
+x = [1000, 60, 500, 50, 888, 500, 2500]
+x.sort()
+print("the largest no in list is {}".format(x[-1]))
