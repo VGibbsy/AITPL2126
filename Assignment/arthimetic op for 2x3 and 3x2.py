678a564dea460d0d9fed98247c84739ccb24123d
VGibbsy <vignesh.s@aialz.com>
2020-08-16 19:19:16 +0530

    Assignment

---
+++ Assignment/arthimetic op for 2x3 and 3x2.py
@@ -0,0 +1,9 @@
+import numpy as np
+x=np.array([[1,2,3],[4,5,6]])
+print(x)
+print("......................")
+y=np.array([[7,8],[9,10],[11,12]])
+print(y)
+print("......................")
+print(x*y)
+#we cant perform this operation because number of rows must be equal to number of colomuns
